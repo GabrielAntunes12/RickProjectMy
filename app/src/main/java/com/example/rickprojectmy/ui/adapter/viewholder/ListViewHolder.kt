2fd5c6a362e4f43bffcb5abb5ac6e73b625f1b24
Gabriel Antunes <gabriel.antunesfernandes@outlook.com>
2021-08-25 14:11:52 -0300

    -- Design Modified details. v2

--- app/src/main/java/com/example/rickprojectmy/ui/adapter/viewholder/ListViewHolder.kt
+++ app/src/main/java/com/example/rickprojectmy/ui/adapter/viewholder/ListViewHolder.kt
@@ -2,8 +2,10 @@ package com.example.rickprojectmy.ui.adapter.viewholder
 
 import android.content.Context
 import android.view.View
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.rickprojectmy.R
 import com.example.rickprojectmy.data.ClickListener
 import com.example.rickprojectmy.data.model.Character
 import kotlinx.android.synthetic.main.character_item.view.*
@@ -14,6 +16,12 @@ class ListViewHolder (itemView : View, var context : Context, private val listen
         itemView.status_character.text = char.status
         Glide.with(context).load(char.image).circleCrop().into(itemView.image_character)
         itemView.relative_layout_click.setOnClickListener { listener.onClick(char.id) }
+
+        if(char.status == "Dead"){
+            itemView.circle.background = AppCompatResources.getDrawable(context, R.drawable.circle_back_dead)
+        }else {
+            itemView.circle.background = AppCompatResources.getDrawable(context, R.drawable.circle_back_char)
+        }
 
     }
 }
