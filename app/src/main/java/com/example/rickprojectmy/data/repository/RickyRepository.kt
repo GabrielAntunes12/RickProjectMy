f3cb231d5bc46b0595e99073f16d28c23fb511eb
Gabriel Antunes <gabriel.antunesfernandes@outlook.com>
2021-08-25 13:11:11 -0300

    -- Design Modified

--- app/src/main/java/com/example/rickprojectmy/data/repository/RickyRepository.kt
+++ app/src/main/java/com/example/rickprojectmy/data/repository/RickyRepository.kt
@@ -1,14 +1,9 @@
 package com.example.rickprojectmy.data.repository
 
-import android.util.Log
-import com.example.rickprojectmy.data.model.CharacterList
-import com.example.rickprojectmy.data.model.Info
-import java.lang.Exception
 
 class RickyRepository(private val rickyService: RickyService) {
 
 
-    suspend fun getRicky() = rickyService.getAllCharacters()
     suspend fun getOneRicky(id : Int) = rickyService.getOneCharacter(id)
     suspend fun getRickyNew(inta : Int) = rickyService.getAllCharactersNew(inta)
 
